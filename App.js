e2fd7b164a9e29220d8a111fe1acb7f0cfd4ae45
EhtishamNazir1234 <ehtishamnazir1234@gmail.com>
2026-02-13 16:44:54 +0500

    initial commit

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{backgroundColor:'plum'}}>Hello World!</Text>
+      <Text style={{height:200,width:200, backgroundColor:'black'}}> </Text>
+      <Text style={{height:200,width:200, backgroundColor:'lightgreen'}}> </Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
